55162a3c4c4710f1378050ce0910a27beb150686
noNameForM3 <stefanrick@gmx.de>
2022-08-29 20:18:42 +0200

    Process cmd line args

--- src/webserv.c
+++ src/webserv.c
@@ -1,6 +1,8 @@
 #include <arpa/inet.h>
+#include <getopt.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -12,12 +14,63 @@
 #define BUF_SIZE 100
 
 // Connect to server using netcat: nc 127.0.0.1 10000
+// Alternatively use the browser, url: localhost:10000
+
+// Add signal handler
+
+/*void test_close_con(int *fp) {
+	close(*fp);
+}
+*/
+
+static const struct option long_options[] = 
+{
+	{ "port",	required_argument, 	0, 'p' },
+	{ "help", 	no_argument,		0, 'h' },
+	{ 0, 0, 0, 0 }
+};
+
+char *help_msg = "Usage: ./webserv [ -p port_number ]\n"
+	     "\t\tCommand summary:\n"
+	     "\t\t\t-h\t\tShow this text\n"
+	     "\t\t\t-p port\t\tSpecify port number for remote connections\n"
+	     "\n\t\tCommand summary (long):\n"
+	     "\t\t\t--help\t\tShow this text\n"
+	     "\t\t\t--port port\tSpecify port number for remote connections\n";
+	     
 
 int main(int argc, char **argv)
 {
 
 	// Add command line parser so user can define custom port number
+	int port = -1;	// to check if user specified port, if not port == -1
+	
+	while(1)
+	{
+		int option_index = 0;
+		int c = getopt_long(argc, argv, "p:h", long_options, &option_index);
 
+		if(c == -1)
+			break;
+
+		switch(c) 
+		{
+			case 'p':
+				port = strtol(optarg, NULL, 10);
+				// possible errors: EINVAL ERANGE (see: man strtol)
+				// valid port number range: 0 - 65536 (add check)
+				// handle errors
+				break;
+			case 'h':
+				printf("%s", help_msg);
+				exit(EXIT_SUCCESS);
+				break;
+			case '?':
+				if(optopt == 'p')	fprintf(stderr, "-p: Missing argument.\n"); 
+				break;
+		}
+	}
+	
 	int socketFd, clientFd=-1;
 	int status, n;
 	struct sockaddr_in6 serv_addr, cli_addr;
@@ -36,7 +89,7 @@ int main(int argc, char **argv)
 	memset(&serv_addr, 0, sizeof(struct sockaddr_in6));
 	serv_addr.sin6_family = AF_INET6;
 	serv_addr.sin6_addr = in6addr_any;
-       	serv_addr.sin6_port = htons(PORT);	
+       	serv_addr.sin6_port = htons( (port == -1) ? PORT : port);	
 
 	// bind
 	status = bind(socketFd, (struct sockaddr*) &serv_addr, sizeof(struct sockaddr_in6));
@@ -54,6 +107,8 @@ int main(int argc, char **argv)
 		close(socketFd);
 	}
 
+	printf("Server started on port: %d\n", (port == -1) ? PORT : port);
+	
 	// accept
 	while(1)
 	{
@@ -73,7 +128,7 @@ int main(int argc, char **argv)
 			// terminate
 			close(socketFd);
 		} 
-		printf("Accepted new connection from: %s:%d ... \n", 
+		printf("Accepted new connection from: %s:%d\n", 
 				cli_addr_str, ntohs(cli_addr.sin6_port));
 			
 		memset(req, '\0', BUF_SIZE);	
@@ -90,7 +145,7 @@ int main(int argc, char **argv)
 			
 		}
 		if(n < 0)
-			return -1;
+			exit(EXIT_FAILURE);
 			
                 if(status == -1)
        		{
@@ -103,11 +158,12 @@ int main(int argc, char **argv)
 
 		snprintf(res, BUF_SIZE, "HTTP/1.0 200 0K");
 		write(clientFd, res, BUF_SIZE);
-		close(clientFd);
-        	printf("Connection closed.\n");
+		//test_close_con(&clientFd);
+        	close(clientFd);
+		printf("Connection closed.\n");
 
 			
 	}
 
-	return 0;
+	exit(EXIT_SUCCESS);
 }
