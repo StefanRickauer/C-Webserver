0d80ee0227a5e8f4699e594ea81908b4c5e08000
noNameForM3 <stefanrick@gmx.de>
2022-08-30 20:06:20 +0200

    Change variable names

--- src/webserv.c
+++ src/webserv.c
@@ -16,8 +16,6 @@
 // Connect to server using netcat: nc 127.0.0.1 10000
 // Alternatively use the browser, url: localhost:10000
 
-// Add signal handler
-
 /*void test_close_con(int *fp) {
 	close(*fp);
 }
@@ -37,12 +35,9 @@ char *help_msg = "Usage: ./webserv [ -p port_number ]\n"
 	     "\n\t\tCommand summary (long):\n"
 	     "\t\t\t--help\t\tShow this text\n"
 	     "\t\t\t--port port\tSpecify port number for remote connections\n";
-	     
 
 int main(int argc, char **argv)
 {
-
-	// Add command line parser so user can define custom port number
 	int port = -1;	// to check if user specified port, if not port == -1
 	
 	while(1)
@@ -71,17 +66,17 @@ int main(int argc, char **argv)
 		}
 	}
 	
-	int socketFd, clientFd=-1;
-	int status, n;
+	int serverFd, clientFd;
+	int status, bytes_read;
 	struct sockaddr_in6 serv_addr, cli_addr;
 	socklen_t addr_len;
 	char cli_addr_str[INET6_ADDRSTRLEN];
-	char req[BUF_SIZE], res[BUF_SIZE];
+	char request[BUF_SIZE], response[BUF_SIZE];
 
 	// create
-	socketFd = socket(AF_INET6, SOCK_STREAM, 0);
+	serverFd = socket(AF_INET6, SOCK_STREAM, 0);
 	
-	if(socketFd == -1)
+	if(serverFd == -1)
 	{
 		// Handle socket error and terminate
 	}
@@ -92,19 +87,19 @@ int main(int argc, char **argv)
        	serv_addr.sin6_port = htons( (port == -1) ? PORT : port);	
 
 	// bind
-	status = bind(socketFd, (struct sockaddr*) &serv_addr, sizeof(struct sockaddr_in6));
+	status = bind(serverFd, (struct sockaddr*) &serv_addr, sizeof(struct sockaddr_in6));
 	if(status == -1)
 	{
 		// Handle bind error, close socket and terminate
-		close(socketFd);
+		close(serverFd);
 	}
 
 	// listen 
-	status = listen(socketFd, BACKLOG);
+	status = listen(serverFd, BACKLOG);
 	if(status == -1)
 	{
 		// Handle listen error, close socket and terminate
-		close(socketFd);
+		close(serverFd);
 	}
 
 	printf("Server started on port: %d\n", (port == -1) ? PORT : port);
@@ -113,38 +108,38 @@ int main(int argc, char **argv)
 	while(1)
 	{
 		addr_len = sizeof(struct sockaddr_in6);
-		clientFd = accept(socketFd, (struct sockaddr*) &cli_addr, &addr_len);
+		clientFd = accept(serverFd, (struct sockaddr*) &cli_addr, &addr_len);
 
 		if(clientFd == -1)
 		{
 			// Handle accept error, close socket and terminate
-			close(socketFd);
+			close(serverFd);
 		}
 
-		if(inet_ntop(AF_INET6, &cli_addr.sin6_addr, cli_addr_str
-					, INET6_ADDRSTRLEN) == NULL)
+		if(inet_ntop(AF_INET6, &cli_addr.sin6_addr, cli_addr_str,
+					INET6_ADDRSTRLEN) == NULL)
 		{
 			// Could not convert client address to string
 			// terminate
-			close(socketFd);
+			close(serverFd);
 		} 
 		printf("Accepted new connection from: %s:%d\n", 
 				cli_addr_str, ntohs(cli_addr.sin6_port));
 			
-		memset(req, '\0', BUF_SIZE);	
+		memset(request, '\0', BUF_SIZE);	
 		
-		while((n = read(clientFd, req, BUF_SIZE-1)) > 0)
+		while((bytes_read = read(clientFd, request, BUF_SIZE-1)) > 0)
 		{
 			
-			fprintf(stdout, "Received: %s\n", req);
+			fprintf(stdout, "Received: %s\n", request);
 			
-			if(req[n-1] == '\n')
+			if(request[bytes_read - 1] == '\n')
 			{
 				break;
 			}
 			
 		}
-		if(n < 0)
+		if(bytes_read < 0)
 			exit(EXIT_FAILURE);
 			
                 if(status == -1)
@@ -154,10 +149,10 @@ int main(int argc, char **argv)
              		continue;
       		}
 
-		memset(res, '\0', BUF_SIZE);
+		memset(response, '\0', BUF_SIZE);
 
-		snprintf(res, BUF_SIZE, "HTTP/1.0 200 0K");
-		write(clientFd, res, BUF_SIZE);
+		snprintf(response, BUF_SIZE, "HTTP/1.0 200 0K");
+		write(clientFd, response, BUF_SIZE);
 		//test_close_con(&clientFd);
         	close(clientFd);
 		printf("Connection closed.\n");
